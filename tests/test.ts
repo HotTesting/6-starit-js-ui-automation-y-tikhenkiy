f20ffb7b74bedb5190013d2ed5c8212d23e7b7cb
y-tikhenkiy <y.tikhenkiy@gmail.com>
2019-01-20 01:51:11 +0200

    Homework 2 - testing registration form

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,45 @@
+//import assert = require("assert");
+import {expect} from "chai";
+
+
+
 describe('WDIO', function () {
-    it('Should be alive', function () {
+    xit('Should be alive', function () {
         browser.url('/')
         console.log('--Test passed!')
-    })
+    }
+      
+    )
+
+    it('should allert incorrect pass', function () {
+        
+        browser.url('/create_account')
+               
+        const acc_firstname = $('input[name=firstname]');
+        const acc_lastname = $('input[name=lastname]');
+        const acc_email = $("//div[@class='form-group col-md-6 required']//input[@name='email']");
+        const acc_pass = $("//div[@class='form-group col-md-6 required']//input[@name='password']");
+        const acc_confirmpass = $('input[name=confirmed_password]');
+        const acc_countrycode = $('select[name=country_code]')
+            
+        acc_firstname.addValue('test');
+        acc_lastname.addValue('testovych');
+        acc_countrycode.selectByValue('GB');
+        acc_email.setValue('test12@test.test');
+        acc_pass.setValue('123123');
+        acc_confirmpass.setValue('023123');
+    
+        browser.click("button[name=create_account]");
+
+        browser.pause(1000);
+
+        let alert;
+        alert=browser.isExisting("//div[@id='notices']/div[@class='alert alert-danger']");
+        //console.log(alert);
+        expect(alert).to.be.true;
+
+
+    }
+      
+    )
 })
