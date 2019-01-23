2579ab43b4eef4a0e95090d28d18be2a0776fa04
y-tikhenkiy <y.tikhenkiy@gmail.com>
2019-01-23 15:52:28 +0200

    Revert "Revert "1. Add faker to register form tests""
    This reverts commit 6144517655318696b71551bbfdd0f60ff672d0bb.

--- tests/signInMenu.ts
+++ tests/signInMenu.ts
@@ -1,18 +1,22 @@
 import {expect} from "chai";
 
-describe.skip('Some test for Account dropdown menu', function () {
-    it('should check is Account dropdown menu visible', function(){
-        browser.url("/");
+describe('Some test for Account dropdown menu', function () {
+    before(function() {
+        browser.url('/');
+        browser.click('ul li[class="account dropdown"]');
         browser.pause(1000);
-        const dropDown = $('ul li[class="account dropdown"]');
-        expect(dropDown.isVisible()).to.be.true;
-      
+      });
+    
+    it('should check is Account dropdown menu visible', function(){
         
+        const dropDown = $('form[name=login_form]');
+        expect(dropDown.isVisible()).to.be.true;
+              
     })
     
     it('should Log in client from Account dropdown menu', function(){
         
-        browser.click('ul li[class="account dropdown"]');           
+                   
         const acc_email = $("div input[name='email']");
         const acc_pass = $("div input[name='password']");
         
